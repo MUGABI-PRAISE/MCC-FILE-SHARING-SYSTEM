58781064c2195926882fb167e44de45939385b8d
mugabi praise <mugabipraise4@gmail.com>
2025-08-27 15:17:23 +0000

    sockets in action

--- src/hooks/useNotifications.js
+++ src/hooks/useNotifications.js
@@ -18,7 +18,7 @@ export default function useNotifications(token, onEvent) {
     let reconnectTimer;
 
     const connect = () => {
-      const wsUrl = `ws://localhost:8000/ws/notifications/?token=${token}`;
+      const wsUrl = `ws://filesharingsystem-72ul.onrender.com/ws/notifications/?token=${token}`;
       socket = new WebSocket(wsUrl);
       wsRef.current = socket;
 
